7b091ef30f616e4d909d4bcc9025082017e325d0
Developer Ashu <rb6485395@gmail.com>
2025-02-15 14:30:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      {/* This is Navbar  */}
+      <Navbar/>
+      
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='flex bg-slate-500 justify-center'>
+        <ul className='flex justify-center items-center'> 
+            <li className='text-white'>EVENT CALENDER</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
